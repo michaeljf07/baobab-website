3e5552999bbf9b1e8993c946afe68c38e137b5c5
Michael Ferreira <michaeljf.2007@gmail.com>
2024-12-27 18:07:27 -0500

    added functionality when user clicks profil pic

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,20 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation"; // Use usePathname instead of useRouter
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 
 function Navbar() {
     const pathname = usePathname();
     const [isSignedIn, setIsSignedIn] = useState(false); // Mock state for sign-in
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false); // State for dropdown
     const profilePicture = "/logo.png";
 
     const isActive = (path: string) => pathname === path;
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
 
     return (
         <nav className="bg-slate-100 h-20 p-4 flex items-center w-full">
@@ -55,14 +60,32 @@ function Navbar() {
                         Wishlists
                     </Link>
                 </div>
-                <div className="flex items-center mr-12">
+                <div className="flex items-center mr-12 relative">
                     {isSignedIn ? (
-                        <div className="flex items-center space-x-2">
+                        <div className="relative">
                             <img
                                 src={profilePicture}
                                 alt="Profile"
-                                className="w-12 h-12 rounded-full"
+                                className="w-12 h-12 rounded-full cursor-pointer"
+                                onClick={toggleDropdown}
                             />
+                            {isDropdownOpen && (
+                                <div className="absolute text-center right-0 mt-2 w-28 bg-white border rounded shadow-md z-50">
+                                    <Link
+                                        href="/profile"
+                                        className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+                                        Profile
+                                    </Link>
+                                    <button
+                                        onClick={() => {
+                                            setIsSignedIn(false);
+                                            setIsDropdownOpen(false);
+                                        }}
+                                        className="block w-full text-center px-4 py-2 text-gray-700 hover:bg-gray-100">
+                                        Logout
+                                    </button>
+                                </div>
+                            )}
                         </div>
                     ) : (
                         <button
