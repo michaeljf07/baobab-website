a6d3b6f26db8a4c5d8521b881ae0b4e4862f024f
Michael Ferreira <michaeljf.2007@gmail.com>
2025-04-07 20:22:07 -0400

    added contact-card to profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,7 +4,7 @@ import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-
+import ContactInfoPage from "./contact-info/page";
 
 interface UserData {
     _id: string;
@@ -428,6 +428,8 @@ export default function Profile() {
                 </div>
             </div>
 
+            <ContactInfoPage />
+
             {/* Password Management Section */}
             <div className="bg-white rounded-lg shadow-lg p-8 mb-8">
                 <h2 className="text-2xl font-bold mb-6">Password Management</h2>
@@ -504,9 +506,6 @@ export default function Profile() {
                                 Cancel
                             </button>
                         </div>
-                        
-                        
-
                     </div>
                 )}
             </div>
@@ -649,14 +648,12 @@ export default function Profile() {
             </div>
 
             <div className="mt-8 text-center">
-                            <Link
-                                href="/profile/contact-info"
-                                className="text-blue-600 hover:underline text-lg"
-                            >
-                                View or Edit Contact Info
-                            </Link>
-                        
-                        </div>
+                <Link
+                    href="/profile/contact-info"
+                    className="text-blue-600 hover:underline text-lg">
+                    View or Edit Contact Info
+                </Link>
+            </div>
         </div>
     );
 }
